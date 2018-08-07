e0556a3c59c61f6919f4e51435bd94ea86d287ca
Schnebli Zoltan <schnebli.zoltan@gmail.com>
2018-08-07 15:22:18 +0300

    - Players name are now modifiable.

--- app/src/main/java/com/zoli/cruciascore2/score/TableScoreView.java
+++ app/src/main/java/com/zoli/cruciascore2/score/TableScoreView.java
@@ -1,6 +1,8 @@
 package com.zoli.cruciascore2.score;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,13 +12,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zoli.cruciascore2.R;
 import com.zoli.cruciascore2.score.decorators.FirstColumnDivider;
+import com.zoli.cruciascore2.score.decorators.GravityCompoundDrawable;
 
 import java.util.ArrayList;
 
@@ -78,34 +82,47 @@ public class TableScoreView extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(scoreAdapter);
         recyclerView.addItemDecoration(new FirstColumnDivider(getBaseContext()));
+        recyclerView.setFocusable(false);
+        recyclerView.setFocusableInTouchMode(false);
     }
 
     @SuppressLint({"InflateParams", "SetTextI18n"})
     private void setUpFirstRow(int mode) {
         View row;
-        
+
         if (mode == 0) {
             row = getLayoutInflater().inflate(R.layout.two_player_row, null);
 
-            TextView textViewPlayer1 = (TextView) row.findViewById(R.id.score_player1);
-            textViewPlayer1.setText("Player1");
+            EditText editTextPlayer1 = (EditText) row.findViewById(R.id.score_player1);
+            editTextPlayer1.setText("Player1");
+            setTopLeftIcon(editTextPlayer1);
+            setOnclickAndOnFocusListener(editTextPlayer1);
 
-            TextView textViewPlayer2 = (TextView) row.findViewById(R.id.score_player2);
-            textViewPlayer2.setText("Player2");
+            EditText editTextPlayer2 = (EditText) row.findViewById(R.id.score_player2);
+            editTextPlayer2.setText("Player2");
+            setTopLeftIcon(editTextPlayer2);
+            setOnclickAndOnFocusListener(editTextPlayer2);
 
             TextView textViewType = (TextView) row.findViewById(R.id.double_round_indicator);
             textViewType.setText("Type");
+
         } else {
             row = getLayoutInflater().inflate(R.layout.three_player_row, null);
 
-            TextView textViewPlayer1 = (TextView) row.findViewById(R.id.score_player1);
-            textViewPlayer1.setText("Player1");
+            EditText editTextPlayer1 = (EditText) row.findViewById(R.id.score_player1);
+            editTextPlayer1.setText("Player1");
+            setTopLeftIcon(editTextPlayer1);
+            setOnclickAndOnFocusListener(editTextPlayer1);
 
-            TextView textViewPlayer2 = (TextView) row.findViewById(R.id.score_player2);
-            textViewPlayer2.setText("Player2");
+            EditText editTextPlayer2 = (EditText) row.findViewById(R.id.score_player2);
+            editTextPlayer2.setText("Player2");
+            setTopLeftIcon(editTextPlayer2);
+            setOnclickAndOnFocusListener(editTextPlayer2);
 
-            TextView textViewPlayer3 = (TextView) row.findViewById(R.id.score_player3);
-            textViewPlayer3.setText("Player3");
+            EditText editTextPlayer3 = (EditText) row.findViewById(R.id.score_player3);
+            editTextPlayer3.setText("Player3");
+            setTopLeftIcon(editTextPlayer3);
+            setOnclickAndOnFocusListener(editTextPlayer3);
 
             TextView textViewType = (TextView) row.findViewById(R.id.double_round_indicator);
             textViewType.setText("Type");
@@ -113,6 +130,43 @@ public class TableScoreView extends AppCompatActivity {
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.score_list_view_header);
         layout.addView(row, 0);
+    }
+
+    private void setTopLeftIcon(TextView textView) {
+        Drawable innerDrawable = getBaseContext().getResources().getDrawable(R.drawable.ic_baseline_create_14px);
+
+        GravityCompoundDrawable gravityDrawable = new GravityCompoundDrawable(innerDrawable);
+        innerDrawable.setBounds(0, 0, innerDrawable.getIntrinsicWidth(), innerDrawable.getIntrinsicHeight());
+        gravityDrawable.setBounds(0, 0, innerDrawable.getIntrinsicWidth(), innerDrawable.getIntrinsicHeight());
+
+        textView.setCompoundDrawables(gravityDrawable, null, null, null);
+    }
+
+    private void setOnclickAndOnFocusListener(final EditText editText) {
+        editText.setFocusableInTouchMode(false);
+        editText.setFocusable(false);
+        editText.setFocusableInTouchMode(true);
+        editText.setFocusable(true);
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                if (!hasFocus) {
+                    if (inputMethodManager != null) {
+                        editText.clearFocus();
+                        editText.setText(editText.getText());
+                        editText.setSelection(0);
+                        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    }
+                } else {
+                    if (inputMethodManager != null) {
+                        editText.setSelection(0);
+                        inputMethodManager.showSoftInput(v, 0);
+                    }
+                }
+            }
+        });
     }
 
     private void updateRecycleView() {
---
+++ app/src/main/java/com/zoli/cruciascore2/score/decorators/GravityCompoundDrawable.java
@@ -0,0 +1,52 @@
+package com.zoli.cruciascore2.score.decorators;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class GravityCompoundDrawable extends Drawable {
+
+    private final Drawable mDrawable;
+
+    public GravityCompoundDrawable(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return mDrawable.getIntrinsicWidth();
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return mDrawable.getIntrinsicHeight();
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        int halfCanvas= canvas.getHeight() / 2;
+        int halfDrawable = mDrawable.getIntrinsicHeight() / 2;
+
+        canvas.save();
+        canvas.translate(0, -halfCanvas + halfDrawable);
+        mDrawable.draw(canvas);
+        canvas.restore();
+    }
+
+    @Override
+    public void setAlpha(int i) {
+        mDrawable.setAlpha(i);
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        mDrawable.setColorFilter(colorFilter);
+    }
+
+    @Override
+    public int getOpacity() {
+        return mDrawable.getOpacity();
+    }
+}
